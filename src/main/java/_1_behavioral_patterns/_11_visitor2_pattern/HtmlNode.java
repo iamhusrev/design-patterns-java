52031e45adb2eccf1a193f63d071053b33239b94
iamhusrev <iamhusrev@gmail.com>
2026-02-22 00:46:46 +0300

    Remove visitor pattern implementation and related classes

--- src/main/java/_1_behavioral_patterns/_11_visitor2_pattern/HtmlNode.java
+++
@@ -1,5 +0,0 @@
-package _1_behavioral_patterns._11_visitor2_pattern;
-
-public interface HtmlNode {
-    void execute(Operation operation);
-}
--- src/main/java/_1_behavioral_patterns/_11_visitor2_pattern/Operation.java
+++
@@ -1,7 +0,0 @@
-package _1_behavioral_patterns._11_visitor2_pattern;
-
-public interface Operation {
-    void apply(AnchorNode anchor);
-
-    void apply(HeadingNode heading);
-}
